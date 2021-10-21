77b918516efef4b2fa0a5cb957ee019cc4da5493
zetanove <supportoinformatico@confimprenditori.it>
2021-10-21 09:21:26 +0200

    analyzer

---
+++ Capitolo 17/ClassNameAnalyzer/ClassNameAnalyzer/ClassNameAnalyzerCodeFixProvider.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
+
+namespace ClassNameAnalyzer
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(ClassNameAnalyzerCodeFixProvider)), Shared]
+    public class ClassNameAnalyzerCodeFixProvider : CodeFixProvider
+    {
+        private const string title = "Rinomina in Pascal Case";
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(ClassNameAnalyzerAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            // Find the type declaration identified by the diagnostic.
+            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
+
+            // Register a code action that will invoke the fix.
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: title,
+                    createChangedSolution: c => SetPascalNameAsync(context.Document, declaration, c),
+                    equivalenceKey: title),
+                diagnostic);
+        }
+
+        private async Task<Solution> SetPascalNameAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
+        {
+            var identifierToken = typeDecl.Identifier;
+            var newName = Char.ToUpper(identifierToken.Text[0]).ToString();
+            if (identifierToken.Text.Length > 1)
+                newName += identifierToken.Text.Substring(1).ToLowerInvariant();
+
+            // ottiene il simbolo che rappresenta il tipo da rinominare.
+            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
+            var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
+
+            // Produce una nuova soluzione con il tipo rinominato.
+            var originalSolution = document.Project.Solution;
+            var optionSet = originalSolution.Workspace.Options;
+            var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
+            return newSolution;
+        }
+
+
+        private async Task<Solution> MakeUppercaseAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
+        {
+            // Compute new uppercase name.
+            var identifierToken = typeDecl.Identifier;
+            var newName = identifierToken.Text.ToUpperInvariant();
+
+            // Get the symbol representing the type to be renamed.
+            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
+            var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
+
+            // Produce a new solution that has all references to that type renamed, including the declaration.
+            var originalSolution = document.Project.Solution;
+            var optionSet = originalSolution.Workspace.Options;
+            var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
+
+            // Return the new solution with the now-uppercase type name.
+            return newSolution;
+        }
+    }
+}
---
+++ Capitolo 17/CompilerTest/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Linq;
+
+namespace CompilerTest
+{
+    class Program
+    {
+        private const string code = @"using System;
+                using System.Collections;
+                using System.Linq;
+                using System.Text;
+
+                namespace HelloWorld
+                {
+                    class Program
+                    {
+                        static void Main(string[] args)
+                        {
+                            Console.WriteLine(""Hello, World!"");
+                        }
+                    }
+                }";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(code);
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+            foreach (var us in root.Usings)
+            {
+                Console.WriteLine(us.ToString());
+            }
+
+            var firstMember = root.Members[0];
+            var helloWorldDeclaration = (NamespaceDeclarationSyntax)firstMember;
+
+            SyntaxKind kind = firstMember.Kind();
+
+            var programDeclaration = (ClassDeclarationSyntax)helloWorldDeclaration.Members[0];
+            var mainDeclaration = (MethodDeclarationSyntax)programDeclaration.Members[0];
+            var argsParameter = mainDeclaration.ParameterList.Parameters[0];
+
+            Console.WriteLine($@"Il metodo {mainDeclaration.Identifier} ha il parametro {argsParameter.Identifier} di tipo {argsParameter.Type} e restituisce {mainDeclaration.ReturnType}");
+
+            tree = CSharpSyntaxTree.ParseText(code);
+            var diagnostics = tree.GetDiagnostics();
+
+            var methods = root.DescendantNodes().OfType<MethodDeclarationSyntax>().Where(m => m.Identifier.Text.StartsWith("Method"));
+
+
+            return;
+        }
+    }
+}

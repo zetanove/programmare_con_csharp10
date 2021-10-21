35b0be27bfd6e3e8c0dc446be019f07811254c59
zetanove <supportoinformatico@confimprenditori.it>
2021-10-21 09:28:49 +0200

    long task

---
+++ Capitolo 13 - Threading Async/LongTasks/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Task longTask = Task.Factory.StartNew(() =>
+{
+    Console.WriteLine($"Start long task:");
+    for (int i = 0; i < 100; i++)
+    {
+        Thread.Sleep(100);
+        Console.WriteLine($"in long task: {i}");
+    }
+    Console.WriteLine($"End long task");
+}, TaskCreationOptions.LongRunning);
+
+
+List<Task> tasks = new List<Task>();
+for (int i = 1; i <= 5; i++)
+{
+    int number = i;
+    tasks.Add(Task.Run(() =>
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            Console.WriteLine("{0} {1}", number, new string('.', j));
+            Thread.Sleep(500);
+        }
+    })
+    );
+}
+
+longTask.Wait();

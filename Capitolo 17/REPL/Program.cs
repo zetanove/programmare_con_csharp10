8dfbf80a2f714137a85ffcdc51f4507f4528d2fe
zetanove <supportoinformatico@confimprenditori.it>
2021-10-21 09:22:01 +0200

    vs extension REPL Scripting API

---
+++ Capitolo 17/REPL/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System;
+using System.Threading.Tasks;
+
+namespace REPL
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            var options = new CSharpParseOptions(LanguageVersion.CSharp8,
+                DocumentationMode.Parse, SourceCodeKind.Script, null);
+
+            Script<object> script = CSharpScript.Create(string.Empty);
+            Console.Write("C# 8 REPL (inserisci :q per uscire)");
+            while (true)
+            {
+                Console.Write("> ");
+                var code = Console.ReadLine();
+                var newScript = script.ContinueWith(code);
+
+                ScriptState<object> result = null;
+                try
+                {
+                    bool isComplete = IsCompleteSubmission(code);
+                    if (code == ":q")
+                        break;
+                    else if (!isComplete)
+                        Console.WriteLine("Istruzione incompleta");
+                    else
+                    {
+                        result = await newScript.RunAsync();
+                        script = newScript;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    var error = ex.ToString();
+                    WriteError(error);
+                }
+
+                if (result != null && result.ReturnValue != null)
+                    Console.WriteLine(result.ReturnValue);
+
+                static void WriteError(string error)
+                {
+                    var color = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(error);
+                    Console.ForegroundColor = color;
+                }
+
+                bool IsCompleteSubmission(string code)
+                {
+                    var syntaxTree = SyntaxFactory.ParseSyntaxTree(code, options: options);
+                    return SyntaxFactory.IsCompleteSubmission(syntaxTree);
+                }
+            }  
+        }
+
+        
+    }
+}
---
+++ Capitolo 17/ScriptingApi/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace ScriptingApi
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            var result = await CSharpScript.EvaluateAsync("1 + 2");
+            Console.WriteLine(result); // 1 + 2 sarà valutato come 3
+
+            double d = await CSharpScript.EvaluateAsync<double>("5/(3-1)*3.5");
+            Console.WriteLine(d); // 7
+
+            var sum = await CSharpScript.EvaluateAsync("int x=1; int y=2; int z=x+y; z");
+            Console.WriteLine(sum); // 3
+
+            try
+            {
+                await CSharpScript.EvaluateAsync("1+a");
+            }
+            catch (CompilationErrorException e)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, e.Diagnostics)); //stampa gli errori di compilazione
+            }
+
+            var state = await CSharpScript.RunAsync("int x=0;int y=x+1; y");
+            ScriptVariable y = state.Variables.First(sv => sv.Name == "y");
+            var returnVal = state.ReturnValue;
+
+
+            ScriptOptions scriptOptions = ScriptOptions.Default;
+            var systemCore = typeof(System.Linq.Enumerable).Assembly;
+            //Aggiunge riferimento
+            scriptOptions = scriptOptions.AddReferences(systemCore);
+            //Aggiunge namespaces
+            scriptOptions = scriptOptions.AddImports("System");
+            scriptOptions = scriptOptions.AddImports("System.Linq");
+            scriptOptions = scriptOptions.AddImports("System.Collections.Generic");
+
+            var state1 = await CSharpScript.RunAsync(@"var list = new List<int>(){1,2,3,4,5};", scriptOptions);
+            state1 = await state1.ContinueWithAsync("var sum = list.Sum();");
+            var sum1 = state1.Variables.FirstOrDefault(v => v.Name == "sum");
+            Console.WriteLine($"sum={sum1.Value}");
+
+        }
+    }
+}

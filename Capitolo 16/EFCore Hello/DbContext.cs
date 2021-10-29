03017a22393bce151d643ec7eeea41a5c8373ddd
zetanove <supportoinformatico@confimprenditori.it>
2021-10-29 17:57:09 +0200

    EF Core sqlite

---
+++ Capitolo 16/EFCore Hello/DbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_Hello
+{
+    public class CarContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=cars.db");
+        }
+    }
+
+}
---
+++ Capitolo 16/EFCore Hello/EntityModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace EFCore_Hello
+{
+    [Table("Car")]
+    public class Car
+    {
+        [Key]
+        public int CarId { get; set; }
+        public string Targa { get; set; }
+
+        [Required]
+        public string Modello { get; set; }
+        public int? PersonId { get; set; }
+
+        public Person Person { get; set; }
+    }
+
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+}

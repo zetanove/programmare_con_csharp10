36025d55594b5dcb80630073c505dd3cfed79c55
zetanove <supportoinformatico@confimprenditori.it>
2021-10-29 17:59:06 +0200

    create database

--- Capitolo 16/EFCore Hello/Program.cs
+++ Capitolo 16/EFCore Hello/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EFCore_Hello;
+
 Console.WriteLine("Hello, EF Core!");
 
+using (var db = new CarContext())
+{
+	bool created = await db.Database.EnsureCreatedAsync();
+	Console.WriteLine(created ? "Database creato" : "Database esistente");
+}

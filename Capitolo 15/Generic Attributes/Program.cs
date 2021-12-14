f01dec80e68d794ebee2219c1e4c425812a1ce66
zetanove <supportoinformatico@confimprenditori.it>
2021-12-14 17:58:02 +0100

    generic attributes

---
+++ Capitolo 15/Generic Attributes/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Attributi generici!");
+
+var attribytes=typeof(MiaClasse).GetCustomAttributes(false);
+
+foreach (var attri in attribytes)
+{
+    Console.WriteLine(attri);
+}
+
+[AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+public class Attr<T> : Attribute
+{ }
+
+[Attr<string>]
+public class MiaClasse
+{
+    [Attr<string>]
+    [Attr<int>]
+    public void Method()
+    {
+    }
+}

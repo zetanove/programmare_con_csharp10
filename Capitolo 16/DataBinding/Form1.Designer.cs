6827ee3e031a02f8f3681ccb1404e5e34f2f7d0f
zetanove <supportoinformatico@confimprenditori.it>
2022-01-10 11:02:40 +0100

    data binding streams

---
+++ Capitolo 16/DataBinding/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DataBinding
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(201, 64);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+    }
+}
---
+++ Capitolo 16/Stream/Program.cs
@@ -0,0 +1,25 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        string path = "c:\\temp\\file.txt";
+
+        using (Stream s = new FileStream(path, FileMode.OpenOrCreate))
+        {
+            s.Write(new byte[] { 1 }, 0, 1);
+        }
+
+        using StreamReader sr1 = File.OpenText(path);
+        string line;
+        while ((line = sr1.ReadLine()) != null)
+        {
+            Console.WriteLine(line);
+        }
+        sr1.Close();
+
+        using StreamReader sr = File.OpenText(path) ;
+        string str = sr.ReadToEnd();
+        
+    }
+}

5c23b176a34c5b528a6761c450b25482f32e6f5e
zetanove <supportoinformatico@confimprenditori.it>
2022-01-10 11:02:03 +0100

    record sealed tostring

--- Capitolo 08/Sealed Record ToString/Program.cs
+++ Capitolo 08/Sealed Record ToString/Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Point pt=new Point(1,2);
-Console.WriteLine("Point: "+pt);
+System.Console.WriteLine("Point: "+pt);
 
 Point pt3=new Point3D(1,2,3);
-Console.WriteLine("Point3D: "+pt3);
+System.Console.WriteLine("Point3D: "+pt3);
 
 public record Point(int X,int Y){
     public sealed override string ToString()
@@ -13,10 +13,10 @@ public record Point(int X,int Y){
 }
 
 public record Point3D(int X,int Y, int Z): Point(X,Y){
-    public override string ToString()
-    {
-        return "io sono un punto 3d";
-    }
+    // public override string ToString()
+    // {
+    //     return "io sono un punto 3d";
+    // }
 }
 
 

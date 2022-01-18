d701195547f3c191ce99b2b8d4e00a6a00959d51
zetanove <supportoinformatico@confimprenditori.it>
2022-01-18 12:17:10 +0100

    Hello World in Visual Studio

---
+++ Capitolo 02/HelloWorldVS/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+ * Programmare con C# 10 guida completa
+ * https://amzn.to/3qEZxae
+ * Autore: Antonio Pelleriti
+ * Capitolo 2: Hello World in Visual Studio
+ */
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");

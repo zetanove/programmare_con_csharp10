111e3b9f9f15e4a0a1383b26447749317c0e93aa
zetanove <supportoinformatico@confimprenditori.it>
2022-01-19 14:37:06 +0100

    eliminazione cartella vecchia capitolo 2

--- Capitolo 02/HelloWorld VSCode/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿/*
- * Programmare con C# 10 guida completa
- * https://amzn.to/3qEZxae
- * Autore: Antonio Pelleriti
- * Capitolo 2: Hello World in Visual Studio Code
- */
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World in VS Code!");
--- Capitolo 02/HelloWorldVS/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿/*
- * Programmare con C# 10 guida completa
- * https://amzn.to/3qEZxae
- * Autore: Antonio Pelleriti
- * Capitolo 2: Hello World in Visual Studio
- */
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");

a4f327b7d9dceecf3f9a64b6cb3f6d9b0186c9de
zetanove <supportoinformatico@confimprenditori.it>
2022-01-19 14:38:30 +0100

    rename capitolo 08 - Ereditarietà e polimorfismo

---
+++ Capitolo 18/MAUI/HelloMauiApp/Page1.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace HelloMauiApp
+{
+
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Capitolo 4/Conversioni/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-int i = 1;
-nint ni = i;
-int j = (int)ni; // necessita cast

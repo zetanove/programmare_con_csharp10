16af86b2f9bb272743268b32d15cab2302b8de38
zetanove <supportoinformatico@confimprenditori.it>
2022-01-24 18:00:13 +0100

    index, range
    System.Object

---
+++ Capitolo 04 - Tipi e oggetti/IndexRange/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Index e Range");
+
+int[] vettore = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+int secondo = vettore[1];
+int ultimo = vettore[vettore.Length - 1];
+Console.WriteLine($"secondo: {secondo}, ultimo: {ultimo}");
+
+Index index1 = 1;
+Index indexLast = ^1;
+
+secondo = vettore[index1];
+ultimo = vettore[indexLast];
+
+Console.WriteLine($"secondo: {secondo}, ultimo: {ultimo}");
+
+int[] intervallo = vettore[1..^4]; //2, 3, 4, 5, 6
+Console.WriteLine(string.Join(',', intervallo));
+
+intervallo = vettore[^2..^0]; //9, 10
+Console.WriteLine(string.Join(',', intervallo));
+
+var tutti = vettore[..];
+var fino4 = vettore[..4];
+var dal6 = vettore[6..];
+
+Console.WriteLine(string.Join(',', tutti));
+Console.WriteLine(string.Join(',', fino4));
+Console.WriteLine(string.Join(',', dal6));
+
+Range r = 3..^3;
+var fetta = vettore[r];
+Console.WriteLine(string.Join(',', fetta));
+
+ref int refUltimo = ref vettore[indexLast];
+refUltimo = 123;
+ultimo = vettore[indexLast];
+Console.WriteLine($"secondo: {secondo}, ultimo: {ultimo}");
---
+++ Capitolo 04 - Tipi e oggetti/System.Object/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace System.Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //l'oggetto i è di tipo valore, ma anche int deriva da System.Object
+            int i = 123;
+            Console.WriteLine("i.ToString()={0}", i.ToString());
+            int j = 456;
+            Console.WriteLine("i.Equals(j): {0}", i.Equals(j));
+            Console.WriteLine("i.GetType(): {0}", i.GetType());
+
+            //Invocare il metodo GetType con ogni tipo predefinito
+            Console.WriteLine("GetType:");
+
+            Console.WriteLine("{0}", new byte().GetType());
+            Console.WriteLine("{0}", new sbyte().GetType());
+            Console.WriteLine("{0}", new short().GetType());
+            Console.WriteLine("{0}", new ushort().GetType());
+             Console.WriteLine("{0}", new nint().GetType());
+             Console.WriteLine("{0}", new nuint().GetType());
+            Console.WriteLine("{0}", 1.GetType());
+            Console.WriteLine("{0}", 1U.GetType());
+            Console.WriteLine("{0}", 1L.GetType());
+            Console.WriteLine("{0}", 1UL.GetType());
+            Console.WriteLine("{0}", 1.0F.GetType());
+            Console.WriteLine("{0}", 1.0D.GetType());
+            Console.WriteLine("{0}", 1M.GetType());
+            Console.WriteLine("{0}", true.GetType());
+            Console.WriteLine("{0}", 'a'.GetType());
+            Console.WriteLine("{0}", "string".GetType());
+
+
+
+            //boxing e unboxing
+            int num = 123;
+            object box = num;
+            Console.WriteLine("type of box: {0}", box.GetType());
+            int unbox = (int)box;
+            Console.WriteLine("unbox: {0}", unbox);
+
+            //double d = (double)box; questa restituisce un'eccezione
+            num = 456;
+            Console.WriteLine("box: {0}", box);//box contiene ancora 123
+            Console.WriteLine("num: {0}", num);//num diventa 456
+
+            Console.ReadLine();
+        }
+    }
+}

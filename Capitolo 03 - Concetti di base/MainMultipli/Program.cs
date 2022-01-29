e513b17f2d799baaa5474981c5ebc705351b381f
zetanove <antonio.pelleriti@gmail.com>
2022-01-29 16:47:02 +0100

    Main multipli

---
+++ Capitolo 03 - Concetti di base/MainMultipli/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Programmare con C# 8 guida completa
+ * Autore: Antonio Pelleriti
+ * Capitolo 3: Main Multipli
+ * vedi MainMultipli.csproj per impostare
+ * <StartupObject>MainMultipli.Program</StartupObject>
+ * oppure da VS proprietà progetto
+ */
+
+using System;
+
+namespace MainMultipli
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+    class Program2
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 2");
+        }
+    }
+}

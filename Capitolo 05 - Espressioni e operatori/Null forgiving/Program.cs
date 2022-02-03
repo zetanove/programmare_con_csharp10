b4acfd723dbe18eb0bffed34b8f6cef2a28e96bf
zetanove <supportoinformatico@confimprenditori.it>
2022-02-03 17:34:36 +0100

    null forgiving

---
+++ Capitolo 05 - Espressioni e operatori/Null forgiving/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Programmare con C# 10 guida completa
+ * Autore: Antonio Pelleriti
+ * Capitolo 5: null coalescing
+ */
+
+
+
+
+#nullable enable
+string? str = null;
+Console.WriteLine(str.Length); //genera un warning: CS8602 Dereference ofa possibly null reference.
+#nullable restore
+
+Console.WriteLine(str!.Length); //evita il warning ma provoca ugualmente eccezione

0041bf1640b6d7eab177cc789d64aa1f46f32647
zetanove <supportoinformatico@confimprenditori.it>
2022-02-03 17:32:52 +0100

    null coalescing

---
+++ Capitolo 05 - Espressioni e operatori/NullCoalescing/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Programmare con C# 8 guida completa
+ * Autore: Antonio Pelleriti
+ * Capitolo 5: null coalescing
+ */
+
+
+
+object nullobj = null;
+object obj = 123;
+var v1 = nullobj ?? 2; // restituisce 2 perché nullobj è uguale a null
+Console.WriteLine(v1);
+var v2 = obj ?? 2; //restituisce 123, perché obj è diverso da null;
+Console.WriteLine(v1);
+
+//null coalescing a cascata
+string str1 = null;
+string str2 = null;
+string name = str1 ?? str2 ?? "senza nome";
+Console.WriteLine(name);
+Console.ReadLine();

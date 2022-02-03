f1d4fa574fda84350ad10a0752cade8ed6a35392
zetanove <supportoinformatico@confimprenditori.it>
2022-02-03 17:37:00 +0100

    null conditional

---
+++ Capitolo 05 - Espressioni e operatori/DeconstructPattern/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace DeconstructPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("deconstruct pattern");
+
+            //Point pt = new Point(3, 5);
+            Coppia pt = new Coppia(3, 5);
+
+            if (pt is (int x, int y))
+            {
+                Console.WriteLine($"pt è stato decostruito in ({x},{y})");
+            }
+
+            if (pt is (0, 0))
+            {
+                Console.WriteLine($"pt è l'origine");
+            }
+        }
+
+        class Point
+        {
+            public int X { get; }
+            public int Y { get; }
+            //public Point(int x, int y) => (X, Y) = (x, y);
+            public Point(int x,int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            //public void Deconstruct(out int x, out int y) => (x, y) = (X, Y);
+            public void Deconstruct(out int x, out int y)
+            {
+                x = X;
+                y = Y;
+            }
+        }
+
+
+        class Coppia
+        {
+            public int X { get; }
+            public int Y { get; }
+            //public Point(int x, int y) => (X, Y) = (x, y);
+            public Coppia(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            //public void Deconstruct(out int x, out int y) => (x, y) = (X, Y);
+            public void Deconstruct(out int x, out int y)
+            {
+                x = X;
+                y = Y;
+            }
+        }
+    }
+}
---
+++ Capitolo 05 - Espressioni e operatori/NullConditional/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Programmare con C# 10 guida completa
+ * Autore: Antonio Pelleriti
+ * Capitolo 5: null conditional
+ */
+
+string str1 = null;
+string str2 = "matilda";
+
+int? lunghezza = str1?.Length;
+Console.WriteLine(lunghezza);//null
+lunghezza = str2?.Length;
+Console.WriteLine(lunghezza);//7
+
+int len = str1?.Length ?? 0;
+Console.WriteLine(len); //0
+
+Console.ReadLine();

ff08acaedfa7fd5f08bd97992f3ecb3941bb8178
zetanove <supportoinformatico@confimprenditori.it>
2022-02-03 17:31:29 +0100

    ref conditional

---
+++ Capitolo 05 - Espressioni e operatori/RefConditional/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Programmare con C# 10 guida completa
+ * Autore: Antonio Pelleriti
+ * Capitolo 4: operatore ref conditional
+ */
+
+
+var smallArray = new int[] { 1, 2, 3, 4, 5 };
+var largeArray = new int[] { 10, 20, 30, 40, 50 };
+
+int index = 7;
+ref int refValue = ref ((index < 5) ? ref smallArray[index] : ref largeArray[index - 5]);
+refValue = 0;
+
+index = 2;
+((index < 5) ? ref smallArray[index] : ref largeArray[index - 5]) = 100;
+
+Console.WriteLine(string.Join(" ", smallArray));
+
+Console.WriteLine(string.Join(" ", largeArray));// Output: 10 20 0 40 50

d9e63480458f53f2c12ab912a3ddbc4e5e6e95da
zetanove <supportoinformatico@confimprenditori.it>
2022-02-03 16:48:30 +0100

    nullable reference

--- Capitolo 04 - Tipi e oggetti/Nullable Reference/Program.cs
+++ Capitolo 04 - Tipi e oggetti/Nullable Reference/Program.cs
@@ -3,8 +3,9 @@ Console.WriteLine("Hello, World!");
 
 
 #nullable enable
-string? str = null; //genera un warning perchè la variabile non può essere null
-Console.WriteLine(str!.Length); //genera un warning perchè la variabile non può essere null
+string str = null; //genera un warning perchè la variabile non può essere null, sostituire con string? str
+//str="hello"
+Console.WriteLine(str!.Length); //genera un warning perchè la variabile non può essere null, usando str!.length se la variabile è sicuramente non null
 #nullable restore
 
 string str2 = null;

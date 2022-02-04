20008e0cded25ff6a1d84b45d7ffa6c183dd738c
zetanove <supportoinformatico@confimprenditori.it>
2022-02-04 09:56:34 +0100

    struct

--- Capitolo 07 - OOP/Structs/Program.cs
+++ Capitolo 07 - OOP/Structs/Program.cs
@@ -4,42 +4,52 @@
  * Capitolo 6: strutture
  */
 
-using System;
+
+using Structs;
+
+Point pt = new Point();
+Console.WriteLine("pt: {0}, {1}", pt.X, pt.Y);
+
+Point pt2 = pt;
+Console.WriteLine("pt2: {0}, {1}", pt2.X, pt2.Y);
+pt2.X = 11;
+pt2.Y = 22;
+Console.WriteLine("pt: {0}, {1}", pt.X, pt.Y);
+Console.WriteLine("pt2: {0}, {1}", pt2.X, pt2.Y);
+
+pt = new Point(1, 1);
+pt2 = new Point(3, 4);
+Point sum = pt.AddToPoint(pt2);
+
+Console.WriteLine("pt: {0}, {1}", pt.X, pt.Y);
+Console.WriteLine("pt2: {0}, {1}", pt2.X, pt2.Y);
+
+//struct per riferimento
+pt = new Point(1, 1);
+pt2 = new Point(3, 4);
+pt.AddToPoint(ref pt2);
+
+Console.WriteLine("pt: {0}, {1}", pt.X, pt.Y);
+Console.WriteLine("pt2: {0}, {1}", pt2.X, pt2.Y);
 
 
-namespace Structs
+
+//C# 10: espressione with con struct
+Point pt3 = pt with { X = 123 };
+Console.WriteLine("pt3: {0}, {1}", pt3.X, pt3.Y);
+
+//ref struct
+NuovaStruct ns = new NuovaStruct();
+ns.Num = 123;
+//object o = ns; //ERRORE boxing non possibile
+
+struct Container
 {
-    struct Program
-    {
-        static void Main(string[] args)
-        {
-            Point pt = new Point();
-            Console.WriteLine("pt: {0}, {1}", pt.X, pt.Y);
+    //public NuovaStruct field; //errore, una ref struct non può essere usata come campo di una struct normale
+}
 
-            Point pt2 = pt;
-            Console.WriteLine("pt2: {0}, {1}", pt2.X, pt2.Y);
-            pt2.X = 11;
-            pt2.Y = 22;
-            Console.WriteLine("pt: {0}, {1}", pt.X, pt.Y);
-            Console.WriteLine("pt2: {0}, {1}", pt2.X, pt2.Y);
-
-            pt = new Point(1, 1);
-            pt2 = new Point(3, 4);
-            Point sum = pt.AddToPoint(pt2);
-
-            Console.WriteLine("pt: {0}, {1}", pt.X, pt.Y);
-            Console.WriteLine("pt2: {0}, {1}", pt2.X, pt2.Y);
-
-            pt = new Point(1, 1);
-            pt2 = new Point(3, 4);
-            pt.AddToPoint(ref pt2);
-
-            Console.WriteLine("pt: {0}, {1}", pt.X, pt.Y);
-            Console.WriteLine("pt2: {0}, {1}", pt2.X, pt2.Y);
-
-            Point pt3 = pt with { X = 123 };
-            Console.WriteLine("pt3: {0}, {1}", pt3.X, pt3.Y);
-
-        }
-    }
+ref struct Container2
+{
+    public NuovaStruct field;
+    public Span<int> spanInt;
 }
--- Capitolo 07 - OOP/Structs/Strutture.cs
+++ Capitolo 07 - OOP/Structs/Strutture.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * Programmare con C# 10 guida completa
+ * https://amzn.to/3qEZxae
+ * Autore: Antonio Pelleriti
+ * Capitolo 7: struct
+ */
+using System;
 
 namespace Structs
 {
     public struct Point
     {
-        public double X { get; set; }
-        public double Y { get; set; }
+        public double X { get; set; } = 0;
+        public double Y { get; set; } = 0;
         public readonly double Distance => Math.Sqrt(X * X + Y * Y);
 
         // public readonly void Reset(int x) => X = x; //errore, non si può assegnare a X
@@ -30,6 +32,9 @@ namespace Structs
             return pt;
         }
 
+        public Point() //errore prima di C# 10, costruttore senza parametri esplicito
+        { }
+
         //Questo genera errore di compilazione in C# 8, in quanto è readonly ma tenta di modificare lo stato
         //public readonly void Sposta(Point pt)
         //{
@@ -45,6 +50,19 @@ namespace Structs
         }
     }
 
+    public struct Point2
+    {
+        public double X;
+        public double Y;
+
+        //errore, Y non è inizializzato
+        //public Point2(double x)
+        //{
+        //    this.X = x;
+        //}
+    }
+
+    //readonly struct
     public readonly struct ReadonlyPoint
     {
         public readonly double X;
@@ -53,9 +71,11 @@ namespace Structs
 
         public ReadonlyPoint(double x, double y) => (X, Y) = (x, y);
         public override string ToString() => $"({X}, {Y})";
-
-        
     }
 
-
+    //ref struct
+    ref struct NuovaStruct
+    {
+        public int Num;
+    }
 }

e316c9c5a0dd2019746ec781ba8a92a2a94be30b
zetanove <supportoinformatico@confimprenditori.it>
2022-02-08 09:19:56 +0100

    Json Serialization Esercizi

---
+++ Capitolo 14 - XML e JSON/Esercizi/Ex_14.5/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.Json;
+using Ex_14._5;
+
+Studente s=new Studente("Antonio", "Pelleriti", "180777");
+
+string json = JsonSerializer.Serialize(s);
+Console.WriteLine(json);
---
+++ Capitolo 14 - XML e JSON/Esercizi/Ex_14.5/Studente.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_14._5
+{
+    public record Studente(string Nome, string Cognome, string Matricola);
+}

cde93d28a7a9ec038635a50908b9efda1ea0aa74
zetanove <supportoinformatico@confimprenditori.it>
2022-02-14 15:14:50 +0100

    esercizi

---
+++ Capitolo 05 - Espressioni e operatori/Esercizi/Ex_5.1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Programmare con C# 10
+ * Autore: Antonio Pelleriti
+ * capitolo 5
+ * esercizio 1
+ * Scrivere un’applicazione Console che richieda all’utente l’inserimento
+ * di due numeri e ne stampi Somma, Sottrazione, Moltiplicazione e Divisione.
+ */
+
+Console.WriteLine("Inserisci numero 1:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Inserisci numero 2:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"a + b = {a + b}");
+Console.WriteLine($"a - b = {a - b}");
+Console.WriteLine($"a * b = {a * b}");
+Console.WriteLine($"a / b = {(double)a / b}");
---
+++ Capitolo 05 - Espressioni e operatori/Esercizi/Ex_5.2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Programmare con C# 10
+ * Autore: Antonio Pelleriti
+ * capitolo 5
+ * esercizio 2
+ * Scrivere un’applicazione Console che richieda all’utente l’inserimento
+ * di 2 numeri interi, ed esegua le operazioni di AND e OR bit a bit, e stampi
+ * i risultati in formato binario.
+ */
+
+
+Console.WriteLine("Inserisci numero 1:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Inserisci numero 2:");
+int b = int.Parse(Console.ReadLine());
+
+var and = a & b;
+var or = a | b;
+string strAnd = Convert.ToString(and, 2);
+string strOr = Convert.ToString(or, 2);
+
+Console.WriteLine($"a & b = {strAnd}");
+Console.WriteLine($"a | b = {strOr}");

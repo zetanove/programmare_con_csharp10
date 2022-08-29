d8d3e37e648b029a397eaabddf8cf71379ab8567
Antonio Pelleriti <zetanove@users.noreply.github.com>
2022-08-29 09:52:11 +0200

    esercizio 6.2

--- Capitolo 06 - Controllo di flusso/Esercizi/Ex_6.2/Program.cs
+++ Capitolo 06 - Controllo di flusso/Esercizi/Ex_6.2/Program.cs
@@ -1,25 +1,26 @@
-﻿using System;
+﻿/*
+ * Programmare con C# 10 guida completa
+ * Autore: Antonio Pelleriti
+ * Capitolo 6:
+ * Esercizio 6.2) Scrivere un’applicazione Console che richieda all’utente l'inserimento
+   di infiniti numeri, li sommi, ed esca dal programma alla pressione
+   del tasto X. Prima di uscire stampi la somma dei numeri inseriti
+ */
 
-namespace Ex_6._2
+using System;
+
+Console.WriteLine("Inserisci infiniti numeri, per terminare inserisci X");
+int somma = 0;
+string str;
+while (true)
 {
-    class Program
+    str = Console.ReadLine().ToUpper();
+    if (str == "X")
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Inserisci infiniti numeri, per terminare inserisci X");
-            int somma = 0;
-            string str;
-            while(true)
-            {
-                str = Console.ReadLine().ToUpper();
-                if (str=="X")
-                {
-                    break;
-                }
-                somma += int.Parse(str);
-            }
-
-            Console.WriteLine($"Somma dei numeri inseriti = {somma}");
-        }
+        break;
     }
+    somma += int.Parse(str);
 }
+
+Console.WriteLine($"Somma dei numeri inseriti = {somma}");
+
